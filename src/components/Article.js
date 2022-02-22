7de99b0bc1e7db5def3d91de5e5760c78832f75f
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-22 08:47:08 -0800

    complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* uncomment the line below to pass the test */}
-      please pass this test
-    </div>
-  );
+  return <div>please pass this test</div>;
 }
 
 export default Article;
